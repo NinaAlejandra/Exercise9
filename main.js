8ff652eb523841a56fa8422bd920c4090dbfa62f
NinaAlejandra <niinaa___@hotmail.com>
2023-10-24 10:47:11 +0200

    adding autofocus and adding color changing to js

--- main.js
+++ main.js
@@ -3,44 +3,49 @@ let listBox = document.getElementById("listBox");
 let inputField = document.getElementById("inputField");
 
 const clearInput = () => {
-    document.getElementById("inputField").value ="";
+  document.getElementById("inputField").value = "";
 };
 
-addTodoButton.addEventListener("click", function(){
-    const todoItemContainer = document.createElement("div");
-    todoItemContainer.classList.add("todoItemContainer");
+addTodoButton.addEventListener("click", function () {
+  const todoItemContainer = document.createElement("div");
+  todoItemContainer.classList.add("todoItemContainer");
 
-    const paragraph = document.createElement("p");
-    const buttonsContainer = document.createElement("div");
-    buttonsContainer.classList.add("buttonsContainer");
+  const paragraph = document.createElement("p");
+  const buttonsContainer = document.createElement("div");
+  buttonsContainer.classList.add("buttonsContainer");
 
-    const crossOverBtn = document.createElement("button");
-    const deleteBtn = document.createElement("button");
-    
-    paragraph.innerText = inputField.value;
-    crossOverBtn.innerText = "✔";
-    deleteBtn.innerText = "-";
+  const crossOverBtn = document.createElement("button");
+  const deleteBtn = document.createElement("button");
 
-    if(inputField.value === "") {
-        alert("You need to write something before adding to the list!")
-    }
-    else{
-        buttonsContainer.appendChild(crossOverBtn);
-        buttonsContainer.appendChild(deleteBtn);
-       
-        todoItemContainer.appendChild(paragraph); 
-        todoItemContainer.appendChild(buttonsContainer);
-        listBox.appendChild(todoItemContainer);
-    }
+  paragraph.innerText = inputField.value;
+  crossOverBtn.innerText = "✔";
+  deleteBtn.innerText = "-";
+
+  if (inputField.value === "") {
+    alert("You need to write something before adding to the list!");
+  } else {
+    buttonsContainer.appendChild(crossOverBtn);
+    buttonsContainer.appendChild(deleteBtn);
+
+    todoItemContainer.appendChild(paragraph);
+    todoItemContainer.appendChild(buttonsContainer);
+    listBox.appendChild(todoItemContainer);
+  }
+
+  clearInput();
+
+  crossOverBtn.addEventListener("click", function () {
+    paragraph.style.textDecoration = "line-through";
+    paragraph.style.color = "#deae70";
+     });
+    crossOverBtn.addEventListener("dblclick", function () {
+      paragraph.style.textDecoration = "none";
+      paragraph.style.color = "#000000";
    
-    clearInput();
-
-    crossOverBtn.addEventListener("click", function(){
-        paragraph.style.textDecoration = "line-through";
-    })
-    deleteBtn.addEventListener("click", function(){
-        todoItemContainer.removeChild(paragraph);
-        buttonsContainer.removeChild(crossOverBtn);
-        buttonsContainer.removeChild(deleteBtn);
-    })
-})
+  });
+  deleteBtn.addEventListener("click", function () {
+    todoItemContainer.removeChild(paragraph);
+    buttonsContainer.removeChild(crossOverBtn);
+    buttonsContainer.removeChild(deleteBtn);
+  });
+});
